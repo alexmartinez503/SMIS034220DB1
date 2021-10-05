da96fa0fdb5045691b676b8486aad6fedcbbfa0e
Alexis <martinezgomezalexisgeovany@gmail.com>
2021-10-04 20:54:35 -0600

    segunda actualizacion de vistas y uniones

--- vistasyUniones.sql
+++ vistasyUniones.sql
@@ -22,7 +22,7 @@ ON p.CategoryID = c.CategoryID
 INNER JOIN Suppliers s
 ON p.SupplierID= s.SupplierID;
 
-SELECT producto, categoria, proveedor FROM listadoProductos;
+SELECT producto, categoria, proveedor FROM listadoProductos ORDER BY categoria;
 
 --modificar la vista anterior de tal manera que muestre los nombres que estan inclidos o no 
 --en las ordenes 
@@ -32,13 +32,14 @@ AS
 SELECT 
 p.ProductName producto,
 c.CategoryName categoria,
-s.CompanyName proveedor
+od.OrderID orderID
 FROM Products p
-LEFT JOIN Categories c
-ON p.CategoryID = c.CategoryID
-LEFT JOIN Suppliers s
-ON p.SupplierID= s.SupplierID;
+LEFT JOIN  [Order Details] od
+ON p.ProductID= od.ProductID
+INNER JOIN  Categories c
+ON p.CategoryID = c.CategoryID;
 
+SELECT producto, categoria, orderID FROM listadoProductos ORDER BY categoria;
 
 --modificar la vista anterior usando full join 
 ALTER VIEW listadoProductos
@@ -46,9 +47,11 @@ AS
 SELECT 
 p.ProductName producto,
 c.CategoryName categoria,
-s.CompanyName proveedor
+od.OrderID orderID
 FROM Products p
-FULL JOIN Categories c
-ON p.CategoryID = c.CategoryID
-FULL JOIN Suppliers s
-ON p.SupplierID= s.SupplierID;
+FULL JOIN  [Order Details] od
+ON p.ProductID= od.ProductID
+FULL JOIN  Categories c
+ON p.CategoryID = c.CategoryID;
+
+SELECT producto, categoria, orderID FROM listadoProductos ORDER BY categoria;
